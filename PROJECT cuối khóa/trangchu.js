1d189c6fdc556f043444757357515f0344092ced
C4EJS106DOTHEDAT <dat9d3@gmail.com>
2021-04-08 21:46:34 +0700

    update...

--- PROJECT cuối khóa/trangchu.js
+++ PROJECT cuối khóa/trangchu.js
@@ -10,3 +10,17 @@ window.addEventListener('scroll',function(){
 hamburgerMenu.addEventListener('click',function(){
     header.classList.toggle('menu-open')
 })
+
+// function sendEmail() {
+// 	Email.send({
+// 	Host: "smtp.gmail.com",
+// 	Username : "78d6de2ac69ee5",
+// 	Password : "41e867d1d1532d",
+// 	To : 'dat9d3@gmail.com',
+// 	From : "<sender’s email address>",
+// 	Subject : "<email subject>",
+// 	Body : "<email body>",
+// 	}).then(
+// 		message => alert("mail sent successfully")
+// 	);
+// }
