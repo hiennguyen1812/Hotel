9c8d926062744feee49fefdfa121fcbe99f25561
C4EJS106DOTHEDAT <dat9d3@gmail.com>
2021-04-09 10:08:15 +0700

    first commit

---
+++ PROJECT cuối khóa/booking.js
@@ -0,0 +1,16 @@
+// document.getElementById('submit').onclick = function(){
+//         swal("Here's a message!");
+// };
+
+// document.getElementById('form1').addEventListener('submit',myFunction)
+
+// function myFunction(e){
+//     let formData={
+//         name:document.getElementById('name').value,
+//         email:document.getElementById('email').value,
+//         phone:document.getElementById('phone').value,
+//         request:document.getElementById('request').value
+//     }
+//     localStorage.setItem('formData',JSON.stringify(formData));
+//     e.preventDefault();
+// }
---
+++ PROJECT cuối khóa/data/dataKS.js
@@ -0,0 +1,66 @@
+let dataHotel = [
+    {
+        idKS: 1,
+        tenKS: 'Khách sạn Mường Thanh',
+        diaChiKS: '78 Thợ Nhuộm, Hoàn Kiếm, Quận Hoàn Kiếm, Hà Nội, Việt Nam',
+        viTri: 'Ha Noi',
+        giaThue: '200$',
+    },
+    {
+        idKS: 2,
+        tenKS: 'Khách sạn Hà Nội Daewoo',
+        diaChiKS: '360 Kim Mã, Ngọc Khánh, Ba Đình, Hà Nội',
+        viTri: 'Hai Phong',
+        giaThue: '250$',
+    },
+    {
+        idKS: 3,
+        tenKS: 'Khách sạn Halais Ha Noi',
+        diaChiKS: '48 Trần Nhân Tông, Nguyễn Du, Hai Bà Trưng, Hà Nội',
+        viTri: 'Ha Noi',
+        giaThue: '300$',
+    },
+    {
+        idKS: 4,
+        tenKS: 'Khách sạn Hestia Legend',
+        diaChiKS: '53 Hàng Bún, Quận Ba Đình, Hà Nội, Việt Nam',
+        viTri: 'Quang Ninh',
+        giaThue: '200$',
+    },
+    {
+        idKS: 5,
+        tenKS: 'JW Marriott Hotel Hanoi',
+        diaChiKS: '8 Do Duc Duc, Hà Nội, Việt Nam',
+        viTri: 'Hai Phong',
+        giaThue: '250$',
+    },
+    {
+        idKS: 6,
+        tenKS: 'Khách sạn La Casa Hà Nội',
+        diaChiKS: '17 Phạm Đình Hổ, Hai Bà Trưng, Hà Nội',
+        viTri: 'Ha Noi',
+        giaThue: '300$'
+    },
+    {
+        idKS: 7,
+        tenKS: 'Khách sạn Silk Path Boutique Hà Nội',
+        diaChiKS: '21 Hàng Khay, Hoàn Kiếm, Hà Nội',
+        viTri: 'Hai Phong',
+        giaThue: '200$'
+    },
+    {
+        idKS: 8,
+        tenKS: 'Khách sạn Sofitel Legend Metropole Hà Nội',
+        diaChiKS: 'Số 15, Phố Ngô Quyền, Quận Hoàn Kiếm, Quận Hoàn Kiếm, Hà Nội, Việt Nam, ',
+        viTri: 'Quang Ninh',
+        giaThue: '300$'
+    },
+    {
+        idKS: 9,
+        tenKS: 'Khách sạn  Legend Metropole Hà Nội',
+        diaChiKS: 'Số 15, Phố Ngô Quyền, Quận Hoàn Kiếm, Quận Hoàn Kiếm, Hà Nội, Việt Nam, ',
+        viTri: 'Hai Phong',
+        giaThue: '300$'
+    },
+
+]
