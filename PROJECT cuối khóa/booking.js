2528f9e06a9a772562eb747e8bdfdc2b62c60296
C4EJS106DOTHEDAT <dat9d3@gmail.com>
2021-04-12 21:39:16 +0700

    update12/4

--- PROJECT cuối khóa/booking.js
+++ PROJECT cuối khóa/booking.js
@@ -49,7 +49,35 @@ function booking(){
 }
 
 
+const openButton=document.querySelectorAll('[data-modal-target]')
+const closeButton=document.querySelectorAll('[data-close-button]')
+const overlay=document.getElementById('overlay')
 
+openButton.forEach(button =>{
+    button.addEventListener('click', () =>{
+        const modal=document.querySelector(button.dataset.modalTarget)
+        openModal(modal)
+    })
+})
+
+closeButton.forEach(button =>{
+    button.addEventListener('click', () =>{
+        const modal=button.closest('.modal')
+        closeModal(modal)
+    })
+})
+
+function openModal(modal){
+    if(modal==null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+}
+
+function closeModal(modal){
+    if(modal==null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+}
 
 
 
